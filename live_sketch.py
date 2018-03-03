d7339b0c14ff60fbaee4964329271b0a47713729
Jiaju Ma <majiaju@hotmail.com>
2018-03-02 22:47:55 -0500

    another typo fixed

--- live_sketch.py
+++ live_sketch.py
@@ -6,7 +6,7 @@ def sketch(image):
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     #clean up image using Gaussian Blur
-    img_gray_blur = cv2.GaussianBlue(img_gray, (5,5), 0)
+    img_gray_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
 
     #Extract edges
     canny_edges = cv2.Canny(img_gray_blur, 10, 70)
